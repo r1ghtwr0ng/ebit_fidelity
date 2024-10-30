2808e958ab644b8402d7106cd1e327771e599766
r1ghtwr0ng <rightwrong@protonmail.com>
2024-10-30 11:29:00 +0000

    Small tweaks

--- src/network.py
+++ src/network.py
@@ -2,6 +2,7 @@
 import numpy as np
 import netsquid as ns
 
+from collections import deque
 from netsquid.nodes import Node
 from netsquid.qubits import Operator
 from netsquid.components import Component
@@ -13,55 +14,10 @@ from netsquid.examples.repeater_chain import FibreDepolarizeModel
 from netsquid.components.models import FibreDelayModel, FibreLossModel, DepolarNoiseModel
 
 import netsquid.components.instructions as instr
+import netsquid.qubits.ketstates as ks
+from netsquid.qubits import qubitapi as qapi
 
-# Debug prints
-def print_output_message(message, port):
-    # Print qubit, emission time and emission delay.
-    print(f"Qbits ({port}): {message.items}, t={message.meta['emission_time']}, del={message.meta['emission_delay']}")
-
-def debug_output(message, port):
-    results_map = {
-            0: False,
-            1: True,
-            2: True,
-            3: False
-    }
-    status = message.items[0]
-    print(f"Port: {port}, status: {results_map[status]} ({status})")
-
-# Measurement operators for the beamsplitter
-def create_meas_ops(visibility=1):
-    """Sets the photon beamsplitter POVM measurements.
-
-    We are assuming a lot here, see the Netsquid-Physlayer snippet for more info.
-
-    Parameters
-    ----------
-    visibility : float, optional
-        The visibility of the qubits in the detector.
-
-    """
-    mu = np.sqrt(visibility)
-    s_plus = (np.sqrt(1 + mu) + np.sqrt(1 - mu)) / (2. * np.sqrt(2))
-    s_min = (np.sqrt(1 + mu) - np.sqrt(1 - mu)) / (2. * np.sqrt(2))
-    m0 = np.diag([1, 0, 0, 0])
-    ma = np.array([[0, 0, 0, 0],
-                   [0, s_plus, s_min, 0],
-                   [0, s_min, s_plus, 0],
-                   [0, 0, 0, np.sqrt(1 + mu * mu) / 2]],
-                  dtype=complex)
-    mb = np.array([[0, 0, 0, 0],
-                   [0, s_plus, -1. * s_min, 0],
-                   [0, -1. * s_min, s_plus, 0],
-                   [0, 0, 0, np.sqrt(1 + mu * mu) / 2]],
-                  dtype=complex)
-    m1 = np.diag([0, 0, 0, np.sqrt(1 - mu * mu) / np.sqrt(2)])
-    n0 = Operator("n0", m0)
-    na = Operator("nA", ma)
-    nb = Operator("nB", mb)
-    n1 = Operator("n1", m1)
-    return [n0, na, nb, n1]
-
+# ---- CLASSES ----
 class BSMDetector(QuantumDetector):
     """A component that performs Bell basis measurements.
 
@@ -120,7 +76,6 @@ class BSMDetector(QuantumDetector):
 
 class EmitProgram(QuantumProgram):
     """Program to create a qubit and emit an entangled photon to the 'qout' port.
-
     """
     def __init__(self):
         super().__init__(num_qubits=2)
@@ -131,6 +86,63 @@ class EmitProgram(QuantumProgram):
         self.apply(instr.INSTR_INIT, q1)
         self.apply(instr.INSTR_EMIT, [q1, q2])
         yield self.run()
+
+class CorrectZProgram(QuantumProgram):
+    """Program to apply a Pauli Z correction to a shared Bell state
+    """
+    def __init__(self):
+        super().__init__(num_qubits=1)
+
+    def program(self):
+        # Emit from q2 using q1
+        q1 = self.get_qubit_indices(self.num_qubits)
+        self.apply(instr.INSTR_Z, q1)
+        yield self.run()
+
+class CorrectXProgram(QuantumProgram):
+    """Program to apply a Pauli X correction to a shared Bell state
+    """
+    def __init__(self):
+        super().__init__(num_qubits=1)
+
+    def program(self):
+        # Emit from q2 using q1
+        q1 = self.get_qubit_indices(self.num_qubits)
+        self.apply(instr.INSTR_X, q1)
+        yield self.run()
+
+# Measurement operators for the beamsplitter
+def create_meas_ops(visibility=1):
+    """Sets the photon beamsplitter POVM measurements.
+
+    We are assuming a lot here, see the Netsquid-Physlayer snippet for more info.
+
+    Parameters
+    ----------
+    visibility : float, optional
+        The visibility of the qubits in the detector.
+
+    """
+    mu = np.sqrt(visibility)
+    s_plus = (np.sqrt(1 + mu) + np.sqrt(1 - mu)) / (2. * np.sqrt(2))
+    s_min = (np.sqrt(1 + mu) - np.sqrt(1 - mu)) / (2. * np.sqrt(2))
+    m0 = np.diag([1, 0, 0, 0])
+    ma = np.array([[0, 0, 0, 0],
+                   [0, s_plus, s_min, 0],
+                   [0, s_min, s_plus, 0],
+                   [0, 0, 0, np.sqrt(1 + mu * mu) / 2]],
+                  dtype=complex)
+    mb = np.array([[0, 0, 0, 0],
+                   [0, s_plus, -1. * s_min, 0],
+                   [0, -1. * s_min, s_plus, 0],
+                   [0, 0, 0, np.sqrt(1 + mu * mu) / 2]],
+                  dtype=complex)
+    m1 = np.diag([0, 0, 0, np.sqrt(1 - mu * mu) / np.sqrt(2)])
+    n0 = Operator("n0", m0)
+    na = Operator("nA", ma)
+    nb = Operator("nB", mb)
+    n1 = Operator("n1", m1)
+    return [n0, na, nb, n1]
 
 def create_simple_processor(qpu_name, qbit_count=2, depolar_rate=0):
     """Create a quantum processor with basic operations for teleportation."""
@@ -146,32 +158,85 @@ def create_simple_processor(qpu_name, qbit_count=2, depolar_rate=0):
     memory_noise_model = DepolarNoiseModel(depolar_rate=depolar_rate)
     processor = QuantumProcessor(qpu_name, num_positions=qbit_count, memory_noise_models=[memory_noise_model]*2, 
                                  phys_instructions=physical_instructions)
+    processor.add_ports(['correction'])
     return processor
 
+# ---- CALLBACKS ----
+# Callback for scheduling corrections
+def handle_corrections(message, port, qpu_x, qpu_z, qpu_x_queue, qpu_z_queue):
+    status = message.items[0]
+    if status == 1:
+        print(f"Recv: {status} on port: {port}, performing X correction")
+        execute_program_with_queue(qpu_x, CorrectXProgram(), qpu_x_queue)
+    elif status == 2:
+        print(f"Recv: {status} on port: {port}, performing Z correction")
+        execute_program_with_queue(qpu_z, CorrectZProgram(), qpu_z_queue)
+    else:
+        print(f"Recv: {status} on port: {port}, bad state, no corrections performed")
+
+# ---- QUEUE IMPLEMENTATION ----
+# Helper to manage program queue execution
+def execute_program_with_queue(qpu, program, queue):
+    if not qpu.busy:
+        qpu.execute_program(program)
+    else:
+        queue.append(program)
+
+def on_program_done(msg, target_qpu, second_qpu, queue=[]):
+    # Execute the next program in the queue if available
+    if len(queue) > 0:
+        next_program = queue.popleft()
+        print(f"Queuing next program {next_program}")
+        execute_program_with_queue(target_qpu, next_program, queue)
+    try:
+        print(msg, end=' | ')
+        q0 = target_qpu.peek(positions=0)[0]
+        q1 = second_qpu.peek(positions=0)[0]
+        f0 = qapi.fidelity([q0, q1], ks.b00, squared=True)
+        f1 = qapi.fidelity([q0, q1], ks.b01, squared=True)
+        f2 = qapi.fidelity([q0, q1], ks.b10, squared=True)
+        f3 = qapi.fidelity([q0, q1], ks.b11, squared=True)
+        print(f"B00: {f0}, B01: {f1}, B10: {f2}, B11: {f3}")
+    except Exception as e:
+        print(f"Error accessing qubits: {e}")
+
 def run():
+    # Initialize program queues
+    qpu_a_queue = deque()
+    qpu_b_queue = deque()
+
     # Run simulation
     ns.sim_reset()
 
     # Setup BSM detector
-    detector = BSMDetector("test")
-    detector.ports['cout0'].bind_output_handler(lambda msg: debug_output(msg, 'cout0'))
-    detector.ports['cout1'].bind_output_handler(lambda msg: debug_output(msg, 'cout1'))
-
+    detector = BSMDetector("bsm_detector")
+    
     # Instantiate the QPU units for Alice and Bob
     qpu_a = create_simple_processor("qpu_a", 2, 0) 
-    qpu_a.ports['qout'].connect(detector.ports['qin0'])
     qpu_b = create_simple_processor("qpu_b", 2, 0) 
+    qpu_a.ports['qout'].connect(detector.ports['qin0'])
     qpu_b.ports['qout'].connect(detector.ports['qin1'])
+    
+    # Setup event handlers
+    qpu_a.set_program_done_callback(on_program_done, "Callback hit on A", qpu_a, qpu_b, qpu_a_queue)
+    qpu_b.set_program_done_callback(on_program_done, "Callback hit on B", qpu_b, qpu_a, qpu_b_queue)
+    qpu_a.set_program_fail_callback(on_program_done, "Callback (FAIL) hit on A", qpu_a, qpu_b, qpu_a_queue)
+    qpu_b.set_program_fail_callback(on_program_done, "Callback (FAIL) hit on B", qpu_b, qpu_a, qpu_b_queue)
 
+    # Bind the output ports to the QPU so we can do qubit corrections
+    detector.ports['cout0'].connect(qpu_a.ports['correction'])
+    detector.ports['cout1'].connect(qpu_b.ports['correction'])
+    qpu_a.ports['correction'].bind_input_handler(lambda msg: handle_corrections(msg, 'cout0', qpu_a, qpu_b, qpu_a_queue, qpu_b_queue))
+    qpu_b.ports['correction'].bind_input_handler(lambda msg: handle_corrections(msg, 'cout1', qpu_a, qpu_b, qpu_a_queue, qpu_b_queue))
+    
     # Setup & run program
     prog = EmitProgram()
-    qpu_a.execute_program(prog)
-    qpu_b.execute_program(prog)
-    
+    execute_program_with_queue(qpu_a, EmitProgram(), qpu_a_queue)
+    execute_program_with_queue(qpu_b, EmitProgram(), qpu_b_queue)
     stats = ns.sim_run()
     #print(stats)
 
 if __name__ == "__main__":
-    for _ in range(10):
+    for _ in range(100):
         print('-------------------------------')
         run()
