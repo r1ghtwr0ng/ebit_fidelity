4a9fc527763a1ba1ba142ea99cdf13b7358e18a0
r1ghtwr0ng <r1ghtwr0ng@protonmail.com>
2024-11-14 12:56:14 +0000

    Added switch and started work on control flow via msg headers

--- src/network.py
+++ src/network.py
@@ -4,7 +4,7 @@ import netsquid as ns
 
 # Import from own code
 from qpu_entity import QPUEntity
-from detectors import BSMDetector
+from fso_switch import FSOSwitch
 from fidelity_calculator import FidelityCalculator
 
 # from netsquid.examples.repeater_chain import FibreDepolarizeModel
@@ -19,31 +19,33 @@ def run():
     # Create a fidelity calculator component
     calculator = FidelityCalculator("calculator")
 
-    # Integration with the BSMDetector
+    # Integration with the FSOSwitch
     alice = QPUEntity("AliceQPU", correction=False)
     bob = QPUEntity("BobQPU", correction=True)
 
-    # Connect QPU output ports to the detector input
-    detector = BSMDetector("bsm_detector")
-    alice.processor.ports["qout"].connect(detector.ports["qin0"])
-    bob.processor.ports["qout"].connect(detector.ports["qin1"])
+    # Connect QPU output ports to the switch input
+    fsoswitch = FSOSwitch("bsm_fsoswitch")
+    alice.processor.ports["qout_hdr"].connect(fsoswitch.ports["qin0"])
+    bob.processor.ports["qout_hdr"].connect(fsoswitch.ports["qin1"])
 
     # Connect QPUs to the fidelity calculation device
-    alice.processor.ports["qout0"].connect(calculator.ports["qin0"])
-    bob.processor.ports["qout0"].connect(calculator.ports["qin1"])
+    alice.processor.ports["qout0_hdr"].connect(calculator.ports["qin0"])
+    bob.processor.ports["qout0_hdr"].connect(calculator.ports["qin1"])
 
-    # Connect detector correction outputs to QPU correction inputs
-    detector.ports["cout0"].connect(alice.processor.ports["correction"])
-    detector.ports["cout1"].connect(bob.processor.ports["correction"])
+    # Connect fsoswitch correction outputs to QPU correction inputs
+    fsoswitch.ports["cout0"].connect(alice.processor.ports["correction"])
+    fsoswitch.ports["cout1"].connect(bob.processor.ports["correction"])
 
     calculator.ports["qout0"].connect(alice.processor.ports["qin0"])
     calculator.ports["qout1"].connect(bob.processor.ports["qin0"])
 
-    # TODO add quantum fibre channel between QPU and detector
+    # TODO add quantum fibre channel between QPU and fsoswitch
 
     # Start emit programs for both QPUEntities
-    alice.emit()
-    bob.emit()
+    alice_req = 1
+    bob_req = 2
+    alice.emit(alice_req )
+    bob.emit(bob_req)
 
     # Run simulation
     logging.debug("Starting simulation")
@@ -56,7 +58,7 @@ def run():
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     result = []
-    for i in range(1000):
+    for i in range(5):
         logging.debug(f"Starting Run {i}")
         ret = run()
         result += ret
---
+++ src/networking.py
@@ -0,0 +1,5 @@
+class MessageHeader():
+    def __init__(self, id, output_q, output_c):
+        self.id = id
+        self.output_q = output_q
+        self.output_c = output_c
