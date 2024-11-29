8b2b75a33d4656713ae5f2effb0f074789de501e
r1ghtwr0ng <rightwrong@protonmail.com>
2024-11-29 10:41:42 +0000

    Refactor plotting code to seperate module

---
+++ src/plotting.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from utils import distilled_fidelity
+
+
+def plot_fidelity(success_fidelities, fso_depolar_rates, n_values=[1, 2, 3, 4, 5]):
+    # Plot the maximal distilled fidelity for 5 cases against the dephase rate
+    plt.figure(figsize=(10, 6))
+
+    for n in n_values:
+        distilled_fidelities = [distilled_fidelity(f, n) for f in success_fidelities]
+        plt.plot(
+            fso_depolar_rates,
+            distilled_fidelities,
+            marker="none",
+            linestyle="-",
+            label=f"{n} Qubits",
+        )
+
+    plt.xlabel("Dephase probability")
+    plt.ylabel("Distilled fidelity")
+    plt.title("Distilled fidelity vs. dephase probability")
+    plt.grid()
+    plt.legend()
+    plt.savefig("plots/distilled.png")
