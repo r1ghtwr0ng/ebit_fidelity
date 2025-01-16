Fixed fidelity calculation code, cleaned up simulation.py

src/main.py
@@ -24,7 +24,7 @@ def single_sim(
 # TODO add some comments for the parameters
 def main():
     # Set logging level
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger().setLevel(logging.INFO)
 
     # Set switch routing configuration
     # All possible routing configurations, Alice and Bob bindings
@@ -64,11 +64,11 @@ def main():
     switch_routing = {"qin0": "qout0", "qin1": "qout1", "qin2": "qout2"}
 
     fso_depolar_rates = np.linspace(0, 0.5, 2)
-    fso_depolar_rates = [0]
+    fso_depolar_rates = [0.1]
     loss_probabilities = np.linspace(0, 1, 1)
     loss_probabilities = [0]
     qpu_depolar_rate = 0
-    total_runs = 1000
+    total_runs = 100
     plot_data = {}
     results = single_sim(
         total_runs=total_runs,
@@ -77,6 +77,10 @@ def main():
         qpu_depolar_rate=qpu_depolar_rate,
         loss_probabilities=loss_probabilities,
     )
+
+    successful = [f for (success, f) in results[0] if success]
+
+    print(f"Average fidelity: {sum(successful)/len(successful)}")
 
     print("Early exit until I fix plot code")
     return  # Early break for now

src/simulation.py
@@ -4,47 +4,10 @@ import netsquid as ns
 import netsquid.qubits.ketstates as ks
 import netsquid.qubits.qubitapi as qapi
 
-from utils import configure_parameters
+from utils import configure_parameters, get_fidelities
 from qpu_entity import QPUNode
 from fso_switch import FSOSwitch
 from protocols import EntanglementProtocol
-
-
-# Get two qubits at positions 0 for alice and bob and calculate their fidelities
-def get_fidelities(alice, bob):
-    """
-    Calculate the fidelities of entangled qubits for Alice and Bob.
-
-    Parameters
-    ----------
-    alice : QPUNode
-        The QPU entity representing Alice.
-    bob : QPUNode
-        The QPU entity representing Bob.
-
-    Returns
-    -------
-    tuple
-        A tuple containing:
-        - status (bool): True if both Alice and Bob have valid qubits, False otherwise.
-        - fidelity (float): Fidelity of the Bell state |B00>.
-    """
-    status = alice.get_status() and bob.get_status()
-    qubit0 = alice.get_qubit(0)
-    qubit1 = bob.get_qubit(0)
-    fidelities = {
-        "|00>": qapi.fidelity([qubit0, qubit1], np.array([1, 0, 0, 0]), squared=True),
-        "|11>": qapi.fidelity([qubit0, qubit1], np.array([0, 0, 0, 1]), squared=True),
-        "B00": qapi.fidelity([qubit0, qubit1], ks.b00, squared=True),
-        "B01": qapi.fidelity([qubit0, qubit1], ks.b01, squared=True),
-        "B10": qapi.fidelity([qubit0, qubit1], ks.b10, squared=True),
-        "B11": qapi.fidelity([qubit0, qubit1], ks.b11, squared=True),
-    }
-
-    if status:
-        logging.debug(f"[GREPPABLE] Simulation output: {fidelities}")
-
-    return status, fidelities["B00"]
 
 
 def setup_network(model_parameters):
@@ -114,7 +77,7 @@ def single_run(model_parameters, qpu_depolar_rate, switch_routing):
     print(f"FIDELITY: {fidelity}")
 
     # Return results
-    return protocol.results
+    return (protocol.results, fidelity)
 
 
 # Runs the simulation several times, determined by the batch size.