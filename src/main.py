d6fb294d1db823b4313ed60b9e9f7b4b8b348a4c
r1ghtwr0ng <rightwrong@protonmail.com>
2025-04-03 15:40:56 +0000

    Add timestamp to plot output filename

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,8 +36,8 @@ def single_sim(
     # For every loss probability configuration
     for j, loss_prob in enumerate(loss_probabilities):
         # For every depolarization configuration
+        print(f"Progress: {j}/{len(loss_probabilities)}", end="\r")
         for i, fso_drate in enumerate(fso_depolar_rates):
-            print(f"Progress: {i}/{len(fso_depolar_rates)}", end="\r")
             # Generate a model parameter configuration and run simulation batch
             # TODO change back to non-ideal
             model_params = switch_parameters(fso_drate, loss_prob)
@@ -66,71 +67,6 @@ def single_sim(
             ]
 
     return results
-
-
-# TODO add some comments for the parameters
-def main_single():
-    # Set logging level
-    logging.getLogger().setLevel(logging.ERROR)
-
-    # Simulation parameters
-    switch_routing = {"qin0": "qout0", "qin1": "qout1", "qin2": "qout2"}
-
-    # Simulation parameters
-    fso_depolar_rates = np.linspace(0, 0.15, 25)
-    loss_probabilities = np.linspace(0, 0, 1)
-    total_runs = 200
-    max_proto_attempts = 10
-    max_distillations = 3
-
-    # Create a single figure and axes for the plot
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    for max_distill in range(0, max_distillations):
-        # Run simulation and save data (if needed)
-        results = single_sim(
-            total_runs=total_runs,
-            switch_routing=switch_routing,
-            fso_depolar_rates=fso_depolar_rates,
-            loss_probabilities=loss_probabilities,
-            max_attempts=max_proto_attempts,
-            max_distillations=max_distill,
-        )
-
-        # Extract plot metric (assumed to be a 1D array with length matching loss_probabilities)
-        print(f"DEBUG: {results}")
-        plot_metric = results["fidelity"].reshape(-1)
-        main_metric = fso_depolar_rates
-        print(f"DEBUG: plot_metric = {plot_metric}")
-
-        # Plot scatter points
-        ax.scatter(
-            main_metric,
-            plot_metric,
-            marker="o",
-            label=f"{max_distill} distillations",
-        )
-
-        # Compute a best fit line for the current iteration
-        coeffs = np.polyfit(main_metric, plot_metric, deg=1)
-        best_fit = np.poly1d(coeffs)
-        # Generate x-values for the best fit line
-        x_fit = np.linspace(np.min(main_metric), np.max(main_metric), 100)
-        y_fit = best_fit(x_fit)
-        # Plot the best fit line
-        ax.plot(x_fit, y_fit, linestyle="-")
-
-        logging.info("========================================================\n\n\n\n")
-
-    # Set plot labels and legend
-    ax.set_xlabel("Dephase probability")
-    ax.set_ylabel("Average ebit fidelity")
-    ax.set_title("Affect of EPL distillation on ebit fiedelity")
-    ax.legend()
-
-    # Save the figure
-    plt.savefig("plots/2d/distill_plot_fidelity_depol.png")
-    print("\nPlot saved")
 
 
 def main_switching():
@@ -196,9 +132,64 @@ def main_switching():
     ax.legend()
 
     # Save the figure
-    plt.savefig("plots/2d/errbar_plot_simtime.png")
+    plt.savefig(f"plots/2d/errbar_plot_simtime_{int(time.time())}.png")
+    print("\nPlot saved")
+
+
+def main_heatmap():
+    # Simulation parameters
+    switch_routings = [
+        {"qin0": "qout0", "qin1": "qout1", "qin2": "qout2"},  # Low, Low
+        {"qin0": "qout2", "qin1": "qout1", "qin2": "qout0"},  # Low, Mid
+        {"qin0": "qout0", "qin1": "qout2", "qin2": "qout1"},  # Low, High
+        {"qin0": "qout2", "qin1": "qout1", "qin2": "qout0"},  # Mid, Mid
+        {"qin0": "qout2", "qin1": "qout0", "qin2": "qout1"},  # High, Mid
+        {"qin0": "qout1", "qin1": "qout2", "qin2": "qout0"},  # High, High
+    ]
+    titles = [
+        "(Low, Low)",
+        "(Low, Mid)",
+        "(Low, High)",
+        "(Mid, Mid)",
+        "(Mid, High)",
+        "(High, High)",
+    ]
+
+    # Create a figure with 6 subplots (2 rows x 3 columns)
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10), constrained_layout=True)
+
+    for i, switch_routing in enumerate(switch_routings):
+        print(f"Running routing config: {titles[i]}")
+
+        fso_depolar_rates = np.linspace(0, 0.4, 8)
+        loss_probabilities = np.linspace(0, 0.4, 8)
+        total_runs = 60
+        max_proto_attempts = 15
+
+        # Run simulation and save data (if needed)
+        results = single_sim(
+            total_runs=total_runs,
+            switch_routing=switch_routing,
+            fso_depolar_rates=fso_depolar_rates,
+            loss_probabilities=loss_probabilities,
+            max_attempts=max_proto_attempts,
+            max_distillations=1,
+        )
+        heatmap_data = results["fidelity"]
+
+        # Select the appropriate subplot
+        ax = axs[i // 3, i % 3]
+
+        # Plot the heatmap on the current subplot and get the image object
+        im = plot_norm_success(
+            ax, fso_depolar_rates, loss_probabilities, heatmap_data, titles[i]
+        )
+
+    # Add colorbar
+    fig.colorbar(im, ax=axs.ravel().tolist(), label="Success probability", shrink=0.6)
+    plt.savefig(f"plots/heatmaps/new_fidelity_heatmap_{int(time.time())}.png")
     print("\nPlot saved")
 
 
 if __name__ == "__main__":
-    main_single()
+    main_heatmap()
