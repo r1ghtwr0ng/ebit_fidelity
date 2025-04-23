73604b0eff44fb51162e9b3247f93bc633c9a91b
r1ghtwr0ng <rightwrong@protonmail.com>
2025-04-23 10:14:00 +0000

    Fix up code in main

--- src/main.py
+++ src/main.py
@@ -1,40 +1,30 @@
 import os
-import time
 import logging
 import datetime
 import numpy as np
 
 from simulation import batch_run
-from plotting import plot_events, plot_metadata
+from plotting import plot_events, plot_metadata, plot_switch_meta, plot_switch_event
 
 
-# TODO implement a main function with an amplitude dampening parameter
 def main():
     # Set logging level
     logging.getLogger().setLevel(logging.ERROR)
 
     # Switch configuration parameters
     switch_routings = [
-        {"qin0": "qout0", "qin1": "qout1", "qin2": "qout2"},  # Low, Low
-        #    {"qin0": "qout2", "qin1": "qout1", "qin2": "qout0"},  # Low, Mid
-        #    {"qin0": "qout0", "qin1": "qout2", "qin2": "qout1"},  # Low, High
-        #    {"qin0": "qout2", "qin1": "qout1", "qin2": "qout0"},  # Mid, Mid
-        #    {"qin0": "qout2", "qin1": "qout0", "qin2": "qout1"},  # High, Mid
-        #    {"qin0": "qout1", "qin1": "qout2", "qin2": "qout0"},  # High, High
-    ]
-    titles = [
-        "Low_Low",
-        #    "Low_Mid",
-        #    "Low_High",
-        #    "Mid_Mid",
-        #    "Mid_High",
-        #    "High_High",
+        ({"qin0": "qout0", "qin1": "qout1", "qin2": "qout2"}, "low_low"),
+        ({"qin0": "qout2", "qin1": "qout1", "qin2": "qout0"}, "low_mid"),
+        ({"qin0": "qout0", "qin1": "qout2", "qin2": "qout1"}, "low_high"),
+        ({"qin0": "qout2", "qin1": "qout1", "qin2": "qout0"}, "mid_mid"),
+        ({"qin0": "qout2", "qin1": "qout0", "qin2": "qout1"}, "mid_high"),
+        ({"qin0": "qout1", "qin1": "qout2", "qin2": "qout0"}, "high_high"),
     ]
 
     # Simulation sweep parameters
     detector_efficiencies = np.linspace(1, 1, 1)
-    dampening_parameters = np.linspace(0, 0.3, 15)
-    batch_size = 400
+    dampening_parameters = np.linspace(0, 0.3, 7)
+    batch_size = 100
     max_distillations = 3
     max_proto_attempts = 10
     ideal_switch = False
@@ -47,9 +37,13 @@ def main():
     os.mkdir(plot_directory)
     os.mkdir(save_directory)
 
+    # Lists for dataframe collection for bulk plotting between switch configs
+    meta_list = []
+    event_list = []
+
     # Run simulation for each switch configuration
-    for i, switch_routing in enumerate(switch_routings):
-        logging.info(f"Running routing config: {titles[i]}")
+    for switch_routing, title in switch_routings:
+        print(f"Running routing config: {title}")
 
         # Run simulation and save data (if needed)
         df_metadata, df_events = batch_run(
@@ -63,12 +57,25 @@ def main():
         )
 
         # Save to paraquet files on disk
-        df_metadata.to_parquet(f"{save_directory}/df_metadata.parquet")
-        df_events.to_parquet(f"{save_directory}/df_events.parquet")
+        df_metadata.to_parquet(f"{save_directory}/df_metadata_{switch_routing}.parquet")
+        df_events.to_parquet(f"{save_directory}/df_events_{switch_routing}.parquet")
+
+        # Append metadata to list
+        meta_list.append(df_metadata)
+        event_list.append(df_events)
 
         # Plot the collected data
-        plot_metadata(df_metadata, config_name=titles[i], directory=plot_directory)
-        plot_events(df_events, config_name=titles[i], directory=plot_directory)
+        plot_metadata(df_metadata, config_name=title, directory=plot_directory)
+        plot_events(df_events, config_name=title, directory=plot_directory)
+
+    # Get the config names from the routing configurations
+    config_names = [name for _, name in switch_routings]
+
+    # Plot switch configuration differences
+    plot_switch_meta(dfs=meta_list, config_names=config_names, directory=plot_directory)
+    plot_switch_event(
+        dfs=event_list, config_names=config_names, directory=plot_directory
+    )
 
 
 if __name__ == "__main__":
