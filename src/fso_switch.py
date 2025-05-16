08e66211ef282ff1449c4724088f05261addc907
r1ghtwr0ng <r1ghtwr0ng@protonmail.com>
2025-05-16 16:34:38 +0000

    Hooking up FSOSwitch to use the BSMWrapper class (retains request UUID tracking). Refactor amplitude dampening application

--- src/fso_switch.py
+++ src/fso_switch.py
@@ -2,13 +2,12 @@ import json
 import logging
 from utils import loss_prob
 from netsquid.nodes import Node
-from detectors import BSMDetector
+from bsm_wrapper import BSMWrapper
 from netsquid.qubits.qubitapi import amplitude_dampen
 from netsquid.components import QuantumChannel
 from netsquid.components.models import (
     FibreDelayModel,
     FibreLossModel,
-    DepolarNoiseModel,
 )
 
 
@@ -30,7 +29,8 @@ class FSOSwitch(Node):
 
     def __init__(
         self,
-        name,
+        id,
+        ctrl_port,
         dampening_parameter,
         ideal=False,
         herald_ports=["qout0", "qout1"],
@@ -43,17 +43,20 @@ class FSOSwitch(Node):
             "qout0",
             "qout1",
             "qout2",
-            "cout0",
-            "cout1",
+            "cout",
         ]
+        self.id = id
+        name = f"FSO[{id}]"
         super().__init__(name, port_names=ports)
+        self.__ctrl_port = ctrl_port
         self.__setup_fibre_channels(ideal)
         self.__setup_bsm_detector(
             herald_ports=herald_ports,
             det_eff=1,  # Ideal detector
             dampening_parameter=dampening_parameter,
         )
-        self.__setup_port_forwarding()
+        self.__setup_port_forwarding(ctrl_port)
+
         # Amplitude dampening parameter
         self.__amplitude_dampening = dampening_parameter
 
@@ -85,55 +88,22 @@ class FSOSwitch(Node):
             indistinguishability.
         """
 
-        # Create BSMDetector component
-        bsm_detector = BSMDetector(
-            name=f"BSM[{self.name}]",
+        bsm_wrapper = BSMWrapper(
+            name=f"BSMWrap[{self.id}]",
             p_dark=p_dark,
             det_eff=det_eff,
             visibility=visibility,
         )
+        self.add_subcomponent(bsm_wrapper)
 
-        # Add subcomponents
-        self.add_subcomponent(bsm_detector)
-
-        # Define handler functions for applying amplitude dampening before forwarding
-        # Message objects onto BSM device
-        def first_bsm_handler(msg):
-            amplitude_dampen(msg.items[0], self.__amplitude_dampening)
-            logging.debug(
-                f"[FSO | {self.name}] Dampening: {msg.items[0]} by: {self.__amplitude_dampening} | from: {msg.meta['source']}"
-            )
-            bsm_detector.ports["qin0"].tx_input(msg)
-
-        def second_bsm_handler(msg):
-            amplitude_dampen(msg.items[0], self.__amplitude_dampening)
-            logging.debug(
-                f"[FSO | {self.name}] Dampening: {msg.items[0]} by: {self.__amplitude_dampening} | from: {msg.meta['source']}"
-            )
-            bsm_detector.ports["qin1"].tx_input(msg)
-
-        # Connect output heralding ports to BSM device
-        self.ports[herald_ports[0]].bind_output_handler(first_bsm_handler)
-        self.ports[herald_ports[1]].bind_output_handler(second_bsm_handler)
-
-        # Connect classical BSM heralding signal outputs to FSO switch outputs
-        bsm_detector.ports["cout0"].bind_output_handler(self.ports["cout0"].tx_output)
-        bsm_detector.ports["cout1"].bind_output_handler(self.ports["cout1"].tx_output)
-
-    def __setup_port_forwarding(self):
-        """
-        Setup routing for the incoming ports through the lossy channels to the output
-        ports
-        """
-        # Bind input handlers
-        self.ports["qin0"].bind_input_handler(self.__recv_qubit, tag_meta=True)
-        self.ports["qin1"].bind_input_handler(self.__recv_qubit, tag_meta=True)
-        self.ports["qin2"].bind_input_handler(self.__recv_qubit, tag_meta=True)
-
-        # Bind output handlers
-        self.__channels[0].ports["recv"].bind_output_handler(self.__relay_qubit)
-        self.__channels[1].ports["recv"].bind_output_handler(self.__relay_qubit)
-        self.__channels[2].ports["recv"].bind_output_handler(self.__relay_qubit)
+        # Connect ports from FSO to BSM heralding station wrapper
+        self.ports[herald_ports[0]].bind_output_handler(
+            bsm_wrapper.ports["qin0"].tx_input
+        )
+        self.ports[herald_ports[1]].bind_output_handler(
+            bsm_wrapper.ports["qin1"].tx_input
+        )
+        bsm_wrapper.ports["cout"].bind_input_handler(self.__ctrl_port.tx_input)
 
     def __setup_fibre_channels(self, ideal):
         """
@@ -184,20 +154,40 @@ class FSOSwitch(Node):
         # Add subcomponents
         self.__channels = [qchannel_short, qchannel_mid, qchannel_long]
 
+    def __setup_port_forwarding(self, ctrl_port):
+        """
+        Setup routing for the incoming ports through the lossy channels to the output
+        ports
+        """
+        # Bind input handlers
+        self.ports["qin0"].bind_input_handler(self.__recv_qubit, tag_meta=True)
+        self.ports["qin1"].bind_input_handler(self.__recv_qubit, tag_meta=True)
+        self.ports["qin2"].bind_input_handler(self.__recv_qubit, tag_meta=True)
+
+        # Bind output handlers
+        self.__channels[0].ports["recv"].bind_output_handler(self.__relay_qubit)
+        self.__channels[1].ports["recv"].bind_output_handler(self.__relay_qubit)
+        self.__channels[2].ports["recv"].bind_output_handler(self.__relay_qubit)
+
+        # Route COUT messages to CTRL_PORT.tx_input via a lambda
+        self.ports["cout"].bind_output_handler(lambda msg: ctrl_port.tx_input(msg))
+
     def __relay_qubit(self, msg):
         """
-        Route an incoming quantum message to the appropriate output port.
+        Apply amplitude dampening and route an incoming quantum message
+        to the appropriate output port.
 
         Parameters
         ----------
         msg : object
             Quantum message containing metadata for routing.
         """
+
         serialized_headers = msg.meta.get("header", "{}")
         dict_headers = json.loads(serialized_headers)
         outbound_port = dict_headers.pop("outport", None)
         # Debug print
-        logging.debug(f"[FSO | {self.name}] Relaying qubit to port: {outbound_port}")
+        logging.debug(f"{self.name} Relaying qubit to port: {outbound_port}")
 
         # Serialize headers before sending (dict is unhashable)
         msg.meta["header"] = json.dumps(dict_headers)
@@ -205,8 +195,8 @@ class FSOSwitch(Node):
 
     def __recv_qubit(self, msg):
         """
-        Process an inbound qubit, determine the routing path, and forward it
-        through the appropriate lossy channel.
+        Process an inbound qubit, apply amplitude dempaning, determine the routing path
+        and forward it through the appropriate lossy channel.
 
         Parameters
         ----------
@@ -214,20 +204,19 @@ class FSOSwitch(Node):
             Quantum message received on a specific input port.
         """
         inbound_port = msg.meta.get("rx_port_name", "missing_port_name")
-        logging.debug(
-            f"[FSO | {self.name}] Received ({msg.items[0]} sender: {msg.meta['source']}, hdr: {msg.meta['header']}) on port {inbound_port}"
-        )
-        # TODO extract destination from message metadata and route through the
-        # correct channel
         outbound_port = self.__routing_table[inbound_port]
+        logging.debug(
+            f"[{self.name}] Received ({msg.items[0]} sender: {msg.meta['source']}, hdr: {msg.meta['header']}) on port {inbound_port}"
+        )
 
         # Deserialize the JSON headers
         serialized_headers = msg.meta.get("header", "{}")
         dict_headers = json.loads(serialized_headers)
         dict_headers["outport"] = outbound_port
         logging.debug(
-            f"!!! Incoming port: {inbound_port} | Outbound port: {outbound_port}"
+            f"[{self.name}] Incoming: {inbound_port} | Outbound: {outbound_port}"
         )
+
         # Calculate which channel to route through:
         # 0 -> Short channel
         # 1 -> Medium channel
@@ -235,8 +224,13 @@ class FSOSwitch(Node):
         channel_idx = abs(int(inbound_port[-1]) - int(outbound_port[-1]))
         channel = self.__channels[channel_idx]
 
-        # Serialize the headers before sending
+        # Serialize the headers
         msg.meta["header"] = json.dumps(dict_headers)
+
+        # Apply amplitude dampening
+        amplitude_dampen(msg.items[0], self.__amplitude_dampening)
+
+        # Relay qubit
         channel.ports["send"].tx_input(msg)
 
     def switch(self, routing_table):
@@ -260,4 +254,3 @@ class FSOSwitch(Node):
             logging.error(f"[FSO] Invalid routing rable: {routing_table}")
 
         self.__routing_table = routing_table.copy()
-        # TODO set timeout by which you will switch to the next request
